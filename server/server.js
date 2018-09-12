6614e573fcabf657c408576774f442290860ebad
james-owen <james@jamesowen.io>
2018-09-12 11:42:30 -0400

    Deleted all files but CLI

--- server/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`We are listening on port ${PORT}`);
-});
--- server/session/sessionModel.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
